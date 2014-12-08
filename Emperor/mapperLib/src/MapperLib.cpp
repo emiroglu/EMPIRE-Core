6eb3f8f4364e0ff0cd45888fc68368262557042f
Altug Emiroglu <altug@roompc-n18.(none)>
2014-12-08 10:19:10 +0100

    Added comments to MapperLib.h. Mapper library is complete with the exceptions of IGAMortarMapper and dependency on MKL

--- Emperor/mapperLib/src/MapperLib.cpp
+++ Emperor/mapperLib/src/MapperLib.cpp
@@ -1,3 +1,28 @@
+/*  Copyright &copy; 2013, TU Muenchen, Chair of Structural Analysis,
+ *  Stefan Sicklinger, Tianyang Wang, Andreas Apostolatos, Altug Emiroglu Munich
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of EMPIRE.
+ *
+ *  EMPIRE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  EMPIRE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with EMPIRE.  If not, see http://www.gnu.org/licenses/.
+ */
+/***********************************************************************************************//**
+ * \file MapperLib.cpp
+ * This file wraps the C++ API in a C API
+ * \date 5/12/2014
+ **************************************************************************************************/
 #include "AbstractMapper.h"
 #include "NearestNeighborMapper.h"
 #include "NearestElementMapper.h"
@@ -13,38 +38,50 @@ using namespace std;
 /// Lets make an AbstractMapper object map in the global scope
 std::map <std::string, AbstractMapper*> mapperList;
 
-//TODO : add comments
-
-//TODO : before initialization of a mapper check if another one with the same name exists
-
 void init_FE_NearestNeighborMapper(char* mapperName, 
-				int AnumNodes, const double *Anodes, int BnumNodes, const double *Bnodes){
-    
-    std::string mapperNameToMap = std::string(mapperName); 
-    
-    mapperList[mapperNameToMap] = new NearestNeighborMapper(AnumNodes, Anodes, BnumNodes, Bnodes);
+                                   int AnumNodes, const double *Anodes, int BnumNodes, const double *Bnodes){
+    std::string mapperNameToMap = std::string(mapperName);
+
+    if (mapperList.count( mapperNameToMap )){
+        ERROR_OUT("A mapper with name : " + mapperNameToMap + "has already been initialized!");
+        exit(EXIT_FAILURE);
+    }
+    else{
+        mapperList[mapperNameToMap] = new NearestNeighborMapper(AnumNodes, Anodes, BnumNodes, Bnodes);
+    }
 }
 
 void init_FE_NearestElementMapper(char* mapperName,
-			       int AnumNodes, int AnumElems, const int *AnumNodesPerElem, const double *Anodes, const int *AnodeIDs, const int *Aelems, 
-			       int BnumNodes, int BnumElems, const int *BnumNodesPerElem, const double *Bnodes, const int *BnodeIDs, const int *Belems){
+                                  int AnumNodes, int AnumElems, const int *AnumNodesPerElem, const double *Anodes, const int *AnodeIDs, const int *Aelems,
+                                  int BnumNodes, int BnumElems, const int *BnumNodesPerElem, const double *Bnodes, const int *BnodeIDs, const int *Belems){
     
-    std::string mapperNameToMap = std::string(mapperName); 
+    std::string mapperNameToMap = std::string(mapperName);
     
-    mapperList[mapperNameToMap] = new NearestElementMapper(AnumNodes, AnumElems, AnumNodesPerElem, Anodes, AnodeIDs, Aelems, 
-							   BnumNodes, BnumElems, BnumNodesPerElem, Bnodes, BnodeIDs, Belems);
+    if (mapperList.count( mapperNameToMap )){
+        ERROR_OUT("A mapper with name : " + mapperNameToMap + "has already been initialized!");
+        exit(EXIT_FAILURE);
+    }
+    else{
+        mapperList[mapperNameToMap] = new NearestElementMapper(AnumNodes, AnumElems, AnumNodesPerElem, Anodes, AnodeIDs, Aelems,
+                                                               BnumNodes, BnumElems, BnumNodesPerElem, Bnodes, BnodeIDs, Belems);
+    }
 }
 
 void init_FE_BarycentricInterpolationMapper(char* mapperName, 
-					 int AnumNodes, const double *Anodes, int BnumNodes, const double *Bnodes){
+                                            int AnumNodes, const double *Anodes, int BnumNodes, const double *Bnodes){
 
     std::string mapperNameToMap = std::string(mapperName);
     
-    mapperList[mapperNameToMap] = new BarycentricInterpolationMapper(AnumNodes, Anodes, BnumNodes, Bnodes);
+    if (mapperList.count( mapperNameToMap )){
+        ERROR_OUT("A mapper with name : " + mapperNameToMap + "has already been initialized!");
+        exit(EXIT_FAILURE);
+    } else {
+        mapperList[mapperNameToMap] = new BarycentricInterpolationMapper(AnumNodes, Anodes, BnumNodes, Bnodes);
+    }
 }
 
 void init_FE_MortarMapper(char* mapperName,
-			  int AnumNodes, int AnumElems, const int* AnumNodesPerElem, const double* Anodes, const int* AnodeIDs, const int* Aelems,
+                          int AnumNodes, int AnumElems, const int* AnumNodesPerElem, const double* Anodes, const int* AnodeIDs, const int* Aelems,
                           int BnumNodes, int BnumElems, const int* BnumNodesPerElem, const double* Bnodes, const int* BnodeIDs, const int* Belems,
                           int oppositeSurfaceNormal, int dual, int enforceConsistency){
 
@@ -57,44 +94,53 @@ void init_FE_MortarMapper(char* mapperName,
     if (enforceConsistency != 0) _enforceConsistency = true;
 
     std::string mapperNameToMap = std::string(mapperName);
-    
-    mapperList[mapperNameToMap] = new MortarMapper(AnumNodes, AnumElems, AnumNodesPerElem, Anodes, AnodeIDs, Aelems,
-						   BnumNodes, BnumElems, BnumNodesPerElem, Bnodes, BnodeIDs, Belems,
-						   _oppositeSurfaceNormal, _dual, _enforceConsistency);
+
+    if (mapperList.count( mapperNameToMap )){
+        ERROR_OUT("A mapper with name : " + mapperNameToMap + "has already been initialized!");
+        exit(EXIT_FAILURE);
+    } else {
+        mapperList[mapperNameToMap] = new MortarMapper(AnumNodes, AnumElems, AnumNodesPerElem, Anodes, AnodeIDs, Aelems,
+                                                       BnumNodes, BnumElems, BnumNodesPerElem, Bnodes, BnodeIDs, Belems,
+                                                       _oppositeSurfaceNormal, _dual, _enforceConsistency);
+    }
 
 }
 
-//TODO : before doing mapping check if the mapper with the called name exists
 void doConsistentMapping(char* mapperName, int dimension, int dataSize, const double* dataA, double* dataB){
     assert(dimension == 1 || dimension == 3);
     
     std::string mapperNameToMap = std::string(mapperName);
-    
-    // if a vector field is to be mapped x, y, z components are extracted
-    if (dimension == 3){
-        int sizeDataToMap = dataSize/dimension;
 
-        double** dataAtoMap = new double*[dimension];
-        double** dataBtoWrite = new double*[dimension];
+    if (!mapperList.count( mapperNameToMap )){
+        ERROR_OUT("This mapper does not exist : " + mapperNameToMap);
+        exit(EXIT_FAILURE);
+    } else {
+        // if a vector field is to be mapped x, y, z components are extracted
+        if (dimension == 3){
+            int sizeDataToMap = dataSize/dimension;
 
-        for (int i=0; i<dimension ; i++){
-            dataAtoMap[i]=new double[sizeDataToMap];
-            dataBtoWrite[i]=new double[sizeDataToMap];
-        }
-        for (int i=0 ; i<dimension ; i++){
-            for (int j=0 ; j<sizeDataToMap; j++){
-                dataAtoMap[i][j] = dataA[j*dimension+i];
+            double** dataAtoMap = new double*[dimension];
+            double** dataBtoWrite = new double*[dimension];
+
+            for (int i=0; i<dimension ; i++){
+                dataAtoMap[i]=new double[sizeDataToMap];
+                dataBtoWrite[i]=new double[sizeDataToMap];
             }
-            
-            mapperList[mapperNameToMap]->consistentMapping(dataAtoMap[i], dataBtoWrite[i]);
-            for (int j=0 ; j<sizeDataToMap; j++){
-                dataB[j*dimension+i] = dataBtoWrite[i][j];
+            for (int i=0 ; i<dimension ; i++){
+                for (int j=0 ; j<sizeDataToMap; j++){
+                    dataAtoMap[i][j] = dataA[j*dimension+i];
+                }
+
+                mapperList[mapperNameToMap]->consistentMapping(dataAtoMap[i], dataBtoWrite[i]);
+                for (int j=0 ; j<sizeDataToMap; j++){
+                    dataB[j*dimension+i] = dataBtoWrite[i][j];
+                }
             }
         }
-    }
-    // else field is mapped as it is
-    else {
-        mapperList[mapperNameToMap]->consistentMapping(dataA, dataB);
+        // else field is mapped as it is
+        else {
+            mapperList[mapperNameToMap]->consistentMapping(dataA, dataB);
+        }
     }
 }
 
@@ -102,38 +148,46 @@ void doConservativeMapping(char* mapperName, int dimension, int dataSize, const 
     assert(dimension == 1 || dimension == 3);
 
     std::string mapperNameToMap = std::string(mapperName);
-    
-    // if a vector field is to be mapped x, y, z components are extracted
-    if (dimension == 3){
-        int sizeDataToMap = dataSize/dimension;
+    if (!mapperList.count( mapperNameToMap )){
+        ERROR_OUT("This mapper does not exist : " + mapperNameToMap);
+        exit(EXIT_FAILURE);
+    } else {
+        // if a vector field is to be mapped x, y, z components are extracted
+        if (dimension == 3){
+            int sizeDataToMap = dataSize/dimension;
 
-        double** dataBtoMap = new double*[dimension];
-        double** dataAtoWrite = new double*[dimension];
+            double** dataBtoMap = new double*[dimension];
+            double** dataAtoWrite = new double*[dimension];
 
-        for (int i=0; i<dimension ; i++){
-            dataBtoMap[i]=new double[sizeDataToMap];
-            dataAtoWrite[i]=new double[sizeDataToMap];
-        }
-        for (int i=0 ; i<dimension ; i++){
-            for (int j=0 ; j<sizeDataToMap; j++){
-                dataBtoMap[i][j] = dataB[j*dimension+i];
+            for (int i=0; i<dimension ; i++){
+                dataBtoMap[i]=new double[sizeDataToMap];
+                dataAtoWrite[i]=new double[sizeDataToMap];
             }
-            mapperList[mapperNameToMap]->conservativeMapping(dataBtoMap[i], dataAtoWrite[i]);
-            for (int j=0 ; j<sizeDataToMap; j++){
-                dataA[j*dimension+i] = dataAtoWrite[i][j];
+            for (int i=0 ; i<dimension ; i++){
+                for (int j=0 ; j<sizeDataToMap; j++){
+                    dataBtoMap[i][j] = dataB[j*dimension+i];
+                }
+                mapperList[mapperNameToMap]->conservativeMapping(dataBtoMap[i], dataAtoWrite[i]);
+                for (int j=0 ; j<sizeDataToMap; j++){
+                    dataA[j*dimension+i] = dataAtoWrite[i][j];
+                }
             }
         }
-    }
-    // else field is mapped as it is
-    else {
-        mapperList[mapperNameToMap]->conservativeMapping(dataB, dataA);
+        // else field is mapped as it is
+        else {
+            mapperList[mapperNameToMap]->conservativeMapping(dataB, dataA);
+        }
     }
 }   
 
-//TODO : before deletion of a mapper check if a mapper with given name exists
 void deleteMapper(char* mapperName){
-     std::string mapperNameToMap = std::string(mapperName);
-     delete mapperList[mapperNameToMap];
+    std::string mapperNameToMap = std::string(mapperName);
+    if (!mapperList.count( mapperNameToMap )){
+        ERROR_OUT("This mapper does not exist : " + mapperNameToMap);
+        exit(EXIT_FAILURE);
+    } else {
+        delete mapperList[mapperNameToMap];
+    }
 }
 
 void deleteAllMappers(){
@@ -141,8 +195,7 @@ void deleteAllMappers(){
     std::map<std::string, AbstractMapper*>::iterator iter = mapperList.begin();
     
     if(iter!=mapperList.end()){
-      delete iter->second;
-      mapperList.erase(iter);
+        delete iter->second;
+        mapperList.erase(iter);
     }
-    
 }
--- Emperor/mapperLib/src/include/MapperLib.h
+++ Emperor/mapperLib/src/include/MapperLib.h
@@ -1,28 +1,154 @@
+/*  Copyright &copy; 2013, TU Muenchen, Chair of Structural Analysis,
+ *  Stefan Sicklinger, Tianyang Wang, Andreas Apostolatos, Altug Emiroglu Munich
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of EMPIRE.
+ *
+ *  EMPIRE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  EMPIRE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with EMPIRE.  If not, see http://www.gnu.org/licenses/.
+ */
+/***********************************************************************************************//**
+ * \file MapperLib.h
+ * This file defines the EMPIRE Mapper Library API
+ * \date 2/22/2012
+ **************************************************************************************************/
+
 #ifndef MAPPERLIB_H_
 #define MAPPERLIB_H_
 
 #ifdef __cplusplus
 extern "C" { ///Define extern C if C++ compiler is used
 #endif
-//TODO : add comments
-void init_FE_MortarMapper(char* mapperName, int AnumNodes, int AnumElems, const int* AnumNodesPerElem, const double* Anodes, const int* AnodeIDs, const int* Aelems, int BnumNodes, int BnumElems, const int* BnumNodesPerElem, const double* Bnodes, const int* BnodeIDs, const int* Belems, int oppositeSurfaceNormal, int dual, int enforceConsistency);
 
+/***********************************************************************************************
+ * \brief Initializes and inserts a MortarMapper to the mapper list
+ * \param[in] mapperName name of the mapper
+ *
+ * \param[in] AnumNodes number of nodes on slave side
+ * \param[in] AnumElems number of elements on slave side
+ * \param[in] AnumNodesPerElem number of nodes per element on slave side
+ * \param[in] Anodes coordinates of all nodes on slave side
+ * \param[in] AnodeIDs IDs of all nodes on slave side
+ * \param[in] Aelems connectivity table of all elements on slave side
+ *
+ * \param[in] BnumNodes number of nodes on master side
+ * \param[in] BnumElems number of elements on master side
+ * \param[in] BnumNodesPerElem number of nodes per element on master side
+ * \param[in] Bnodes coordinates of all nodes on master side
+ * \param[in] BnodeIDs IDs of all nodes on master side
+ * \param[in] Belems connectivity table of all elements on master side
+ *
+ * \param[in] oppositeSurfaceNormal whether the interface of master side and of master side have opposite normals  or not (true or false)
+ * \param[in] dual whether or not to use dual mortar (true or false)
+ * \param[in] enforceConsistency whether or not to enforce consistency
+ *
+ * \author Altug Emiroglu
+ ***********/
+void init_FE_MortarMapper(char* mapperName,
+                          int AnumNodes, int AnumElems, const int* AnumNodesPerElem, const double* Anodes, const int* AnodeIDs, const int* Aelems,
+                          int BnumNodes, int BnumElems, const int* BnumNodesPerElem, const double* Bnodes, const int* BnodeIDs, const int* Belems,
+                          int oppositeSurfaceNormal, int dual, int enforceConsistency);
+
+/***********************************************************************************************
+ * \brief Initializes and inserts a NearestNeighborMapper to the mapper list
+ * \param[in] mapperName name of the mapper
+ *
+ * \param[in] AnumNodes number of nodes on slave side
+ * \param[in] Anodes coordinates of all nodes on slave side
+ *
+ * \param[in] BnumNodes number of nodes on master side
+ * \param[in] Bnodes coordinates of all nodes on master side
+ *
+ * \author Altug Emiroglu
+ ***********/
 void init_FE_NearestNeighborMapper(char* mapperName, 
-				int AnumNodes, const double *Anodes, int BnumNodes, const double *Bnodes);
+                                   int AnumNodes, const double *Anodes,
+                                   int BnumNodes, const double *Bnodes);
 
+/***********************************************************************************************
+ * \brief Initializes and inserts a NearestElementMapper to the mapper list
+ * \param[in] mapperName name of the mapper
+ *
+ * \param[in] AnumNodes number of nodes on slave side
+ * \param[in] AnumElems number of elements on slave side
+ * \param[in] AnumNodesPerElem number of nodes per element on slave side
+ * \param[in] Anodes coordinates of all nodes on slave side
+ * \param[in] AnodeIDs IDs of all nodes on slave side
+ * \param[in] Aelems connectivity table of all elements on slave side
+ *
+ * \param[in] BnumNodes number of nodes on master side
+ * \param[in] BnumElems number of elements on master side
+ * \param[in] BnumNodesPerElem number of nodes per element on master side
+ * \param[in] Bnodes coordinates of all nodes on master side
+ * \param[in] BnodeIDs IDs of all nodes on master side
+ * \param[in] Belems connectivity table of all elements on master side
+ *
+ * \author Altug Emiroglu
+***********/
 void init_FE_NearestElementMapper(char* mapperName,
-			       int AnumNodes, int AnumElems, const int *AnumNodesPerElem, const double *Anodes, const int *AnodeIDs, const int *Aelems, 
-			       int BnumNodes, int BnumElems, const int *BnumNodesPerElem, const double *Bnodes, const int *BnodeIDs, const int *Belems);
+                                  int AnumNodes, int AnumElems, const int *AnumNodesPerElem, const double *Anodes, const int *AnodeIDs, const int *Aelems,
+                                  int BnumNodes, int BnumElems, const int *BnumNodesPerElem, const double *Bnodes, const int *BnodeIDs, const int *Belems);
 
+/***********************************************************************************************
+ * \brief Initializes and inserts a BarycentricInterpolationMapper to the mapper list
+ * \param[in] mapperName name of the mapper
+ *
+ * \param[in] AnumNodes number of nodes on slave side
+ * \param[in] AnumElems number of elements on slave side
+
+ * \param[in] BnumNodes number of nodes on master side
+ * \param[in] BnumElems number of elements on master side
+ *
+ * \author Altug Emiroglu
+***********/
 void init_FE_BarycentricInterpolationMapper(char* mapperName, 
-					 int AnumNodes, const double *Anodes, int BnumNodes, const double *Bnodes);
+                                            int AnumNodes, const double *Anodes,
+                                            int BnumNodes, const double *Bnodes);
 
+/***********************************************************************************************
+ * \brief Performs consistent mapping on fields (e.g. displacements or tractions) with the previously initialized mapper with name mapperName
+ * \param[in] mapperName name of the mapper
+ *
+ * \param[in] dimension 1 or 3 dimensional data
+ * \param[in] dataSize size of data for fieldA
+ * \param[in] dataA the field of mesh A
+ * \param[out] dataB the field of mesh B
+ * \author Altug Emiroglu
+***********/
 void doConsistentMapping(char* mapperName, int dimension, int dataSize, const double* dataA, double* dataB);
 
+/***********************************************************************************************
+ * \brief Performs conservative mapping on fields (e.g. displacements or tractions) with the previously initialized mapper with name mapperName
+ * \param[in] mapperName name of the mapper
+ *
+ * \param[in] dimension 1 or 3 dimensional data
+ * \param[in] dataSize size of data for fieldB
+ * \param[in] dataB the field of mesh B
+ * \param[out] dataA the field of mesh A
+ * \author Altug Emiroglu
+***********/
 void doConservativeMapping(char* mapperName, int dimension, int dataSize, const double* dataB, double* dataA);
 
+/***********************************************************************************************
+ * \brief Deletes a previously initialized mapper
+ * \param[in] mapperName name of the mapper to be deleted from the mapper list
+ ***********/
 void deleteMapper(char* mapperName);
 
+/***********************************************************************************************
+ * \brief Delets all the initialized mappers in the mapper list
+ ***********/
 void deleteAllMappers();
 
 #ifdef __cplusplus
