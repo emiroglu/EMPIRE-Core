65325b212b17206323fe7382e2c950ca85592de5
Andreas Apostolatos <andreas.apostolatos@tum.de>
2017-07-12 11:01:55 +0200

    Implementation of the projection on 2 2D B-Spline curved finished and tested

--- Emperor/src/CAGD/IGAPatchCurve.h
+++ Emperor/src/CAGD/IGAPatchCurve.h
@@ -74,6 +74,14 @@ public:
     /// Basis related functions
 public:
     /***********************************************************************************************
+     * \brief Returns the Cartesian Coordinates of a point on a B-Spline surface given the basis functions and the knot span index at the parametric location
+     * \param[in/out] _cartesianCoordinates The Cartesian coordinates of the point on the Patch whose basis functions are _locBasisFunctions
+     * \param[in] _knotSpanIndex The knot span index of the knot span where the point lies
+     * \param[in] _locBasisFunctions The non-zero basis functions at the parametric location where the point is
+     ***********/
+    void computeCartesianCoordinates(double* _cartesianCoordinates, int _knotSpanIndex, double* _locBasisFunctions);
+
+    /***********************************************************************************************
      * \brief Returns the Cartesian Coordinates of a point on a NURBS surface whose surface parameters are known
      * \param[in/out] _cartesianCoordinates The Cartesian coordinates of the point on the Patch whose surface parameters are _uPrm
      * \param[in] _uPrm The parameter on the u-coordinate line
@@ -90,6 +98,16 @@ public:
      ***********/
     void computeCartesianCoordinates(double*, double) const;
 
+    /***********************************************************************************************
+     * \brief Returns the base vector components and their derivatives
+     * \param[in/out] _baseVectorAndDerivatives The base vector and its derivatives
+     * \param[in] _knotSpanIndex The knot span index where the given parameter lies in
+     * \param[in] _basisFunctionsAndDerivatives The basis functions and their derivatives
+     * \param[in] _derivOrder The derivative order of the base vector
+     * \author Andreas Apostolatos
+     ***********/
+    void computeBaseVectorAndDerivatives(double* _baseVectorAndDerivatives, int _knotSpanIndex, double* _basisFunctionsAndDerivatives, int _derivOrder) const;
+
     /// Geometric operation functions
 public:
     /***********************************************************************************************
@@ -104,6 +122,18 @@ public:
     bool computeIntersectionsWithKnotBisection(std::vector<double>& _uvSurface , std::vector<double>& _uTilde, unsigned int _dir, double _knot) const;
 
     bool solveIntersectionWithKnotBisection(double* _uvP, double& _uTildeP, double _uTildeP1, double _uTildeP2, unsigned int _dir, double _knot) const;
+
+    /***********************************************************************************************
+     * \brief Computes the point projection on a 2D B-Spline curve. The point and the curve are assumed to be 2D meaning that the 3rd coordinate is not taken in to consideration
+     * \param[in/out] _uPrm The initial guess for the Newton-Raphson algorithm which is overwritten by the parametric coordinates of the projected Point after the function call
+     * \param[in/out] _P The point to be projected, after the function the point is overwritten by the found projection
+     * \param[in] _noCoord The number of coordinates of the given point _p
+     * \param[in] _maxIt Maximum number of iterations for the Newton-Raphson algorithm
+     * \param[in] _tol Convergence tolerance for the Newton-Raphson iterations
+     * \param[out] Boolean on the convergence of the Newton-Raphson algorithm
+     * \author Andreas Apostolatos, Altug Emiroglu
+     */
+    bool computePointProjectionOn2DCurve(double& _uPrm, double* _P, int _noCoord, int _maxIt=MAX_NUM_ITERATIONS_NEWTONRAPHSON, double _tol=TOL_CONVERGENCE_NEWTONRAPHSON);
 
     /// get functions
 public:
@@ -163,6 +193,10 @@ public:
     static int MAX_NUM_ITERATIONS;
     /// The tolerance for the iterations for the computation of the intersection with a line
     static double TOL_CONVERGENCE;
+    /// The maximum number of iterations for the projection of a point on a curve using the Newton-Raphson algorithm
+    static int MAX_NUM_ITERATIONS_NEWTONRAPHSON;
+    /// The tolerance for the iterations for the projection of a point on a curve using the Newton-Raphson algorithm
+    static double TOL_CONVERGENCE_NEWTONRAPHSON;
 
 };
 
